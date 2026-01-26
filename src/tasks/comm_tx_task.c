99d8232bb60c339d439e943fcc448ad26c00b7ba
SashaBrooke <sasha.a.brooke@outlook.com>
2026-01-26 03:22:41 +0000

    feat: use controls freq constant for task rate

--- src/tasks/comm_tx_task.c
+++ src/tasks/comm_tx_task.c
@@ -10,6 +10,8 @@
 #include "task_context.h"
 #include "comms/packet.h"
 #include "hardware_configuration.h"
+#include "software_configuration.h"
+#include "utils/time_utils.h"
 
 void comm_tx_task(void *pvParameters) {
     task_context_t *ctx = (task_context_t *)pvParameters;
@@ -31,6 +33,6 @@ void comm_tx_task(void *pvParameters) {
             gpio_put(TEST_PIN, 0);
         }
 
-        vTaskDelayUntil(&last, 1);
+        vTaskDelayUntil(&last, pdMS_TO_TICKS(SECS2MSECS(FREQ2PERIOD(CONTROLS_FREQ))));
     }
 }
--- src/tasks/sync_task.c
+++ src/tasks/sync_task.c
@@ -5,6 +5,8 @@
 
 #include "sync_task.h"
 #include "hardware_configuration.h"
+#include "software_configuration.h"
+#include "utils/time_utils.h"
 
 void sync_task(void *pvParameters) {
     TickType_t last = xTaskGetTickCount();
@@ -14,6 +16,6 @@ void sync_task(void *pvParameters) {
         gpio_put(TEST_PIN, 1);
         gpio_put(TEST_PIN, 0);
 
-        vTaskDelayUntil(&last, pdMS_TO_TICKS(1));
+        vTaskDelayUntil(&last, pdMS_TO_TICKS(SECS2MSECS(FREQ2PERIOD(CONTROLS_FREQ))));
     }
 }
