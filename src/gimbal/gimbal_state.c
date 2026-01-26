732fbed73e1ceac311358b001d15fb4b2ad47641
SashaBrooke <sasha.a.brooke@outlook.com>
2026-01-26 03:30:06 +0000

    feat: change default gimbal state values

--- src/gimbal/gimbal_state.c
+++ src/gimbal/gimbal_state.c
@@ -2,14 +2,14 @@
 
 // Default gimbal state values
 const bool          GIMBAL_DEFAULT_SAVED_CONFIG_FLAG       = true; // No new settings to save on setup
-const gimbal_mode_e GIMBAL_DEFAULT_SAFE_MODE               = GIMBAL_MODE_FREE; // Safety: Force free mode on powerup
+const gimbal_mode_e GIMBAL_DEFAULT_SAFE_MODE               = GIMBAL_MODE_FREE; // Safety: force free mode on powerup
 const float         GIMBAL_DEFAULT_AXIS_VALUE              = 0.0f; // Axis position/velocity
 const float         GIMBAL_DEFAULT_MOTOR_PWM               = 0.0f;
 const uint8_t       GIMBAL_DEFAULT_MOTOR_DIR               = 0; // TODO: either use as5600 constant or pull from proto file
 const float         GIMBAL_DEFAULT_AXIS_SETPOINT           = GIMBAL_DEFAULT_UNSET_ROM;
 const float         GIMBAL_DEFAULT_AXIS_SOFT_LIMIT         = GIMBAL_DEFAULT_UNSET_ROM;
 const bool          GIMBAL_DEFAULT_GLOBAL_STREAMING_STATUS = false; // Only stream when requested
-const uint16_t      GIMBAL_DEFAULT_STREAM_RATE             = 30; // Hz
+const uint16_t      GIMBAL_DEFAULT_STREAM_RATE             = 0; // Hz (default not streaming, see above)
 
 void setup_gimbal_state(gimbal_state_t *state) {
     state->saved_configuration   = GIMBAL_DEFAULT_SAVED_CONFIG_FLAG;
